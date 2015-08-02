af8f56bc8248a1ae6d7a8ce3b934b2a9391875a6
joyzoso <joyrockon@gmail.com>
2015-08-01 17:18:55 -0700

    Added Menu and Status Bars along with an event to the exit menu item aka "Get Me Outta Here" item.

--- gui1.py
+++ gui1.py
@@ -8,6 +8,7 @@ class Frame(wx.Frame):
         title=title, size=(300,200))
         self.Center()
         self.Move((600,400))
+        self.CreateStatusBar()
 
 
         panel = wx.Panel(self)
@@ -15,6 +16,34 @@ class Frame(wx.Frame):
             size=(100,40), pos=(100,30))
         #Bind button event to the function self.exit
         button.Bind(wx.EVT_BUTTON, self.exit)
+
+        #create menu bar
+        menuBar = wx.MenuBar()
+        #create the menus
+        fileMenu = wx.Menu()
+        editMenu = wx.Menu()
+        self.SetMenuBar(menuBar)
+
+        #add filemenu and editmenu to menuBar
+        menuBar.Append(fileMenu, "File")
+        menuBar.Append(editMenu, "Edit")
+
+        #add items to fileMenu
+        fileMenu.Append(wx.NewId(), "Something New!", \
+                        "Start yourself a new file")
+        fileMenu.Append(wx.NewId(), "Open Me!", \
+                        "Open a file or project")
+        exitItem = fileMenu.Append(wx.NewId(), "Get Me Outta Here!", \
+                        "Exit out of all this mess!")
+        editMenu.Append(wx.NewId(), "Duplicate!", \
+                        "Copy copy copy!")
+        editMenu.Append(wx.NewId(), "Ctrl+V it",\
+                        "Paste that puppy!")
+        editMenu.Append(wx.NewId(), "Cut It Out!", \
+                        "Ctrl+X that!")
+
+        #bind exit menu item to exit function
+        self.Bind(wx.EVT_MENU, self.exit, exitItem)
 
     def exit(self, event):
         self.Destroy()
